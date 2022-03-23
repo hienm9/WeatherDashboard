27b29c5fe4d2e823596fc51c68f397e19fffcf9e
hienm9 <98295316+hienm9@users.noreply.github.com>
2022-03-23 15:41:39 -0400

    update fetch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,23 @@
 // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-let myAPIKey = "78d9272bcac95b3738e5612909e959e6";
-let city = "Columbus";
+const myAPIKey = "78d9272bcac95b3738e5612909e959e6";
+let city = document.getElementById("usercity");
 // let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myAPIKey}&units=imperial`;
+let submitBtn = document.getElementById("btnSubmit");
+
+// add event lister to the submit button
+submitbtn.addEventListener("click", function () {
+  // format of the weather api url
+  let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myAPIKey}&units=imperial`;
 
 // make a get request to url
 fetch(queryURL).then(function(response) {
     // request was successful
     if (response.ok) {
       response.json().then(function(data) {
-        // displayIssues(data);
+        // console(data);
         console.log(data);
+        // display the weather data
+        displayWeather()
       });
     }
     else {
@@ -18,3 +25,7 @@ fetch(queryURL).then(function(response) {
       alert("There was a problem with your request!");
     }
   });
+
+displayWeather(){
+  
+}
