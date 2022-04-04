016f090aa8e15e0dc1881264cb8f71357ae7f2af
hienm9 <98295316+hienm9@users.noreply.github.com>
2022-04-04 03:18:49 -0400

    udpate js, added pseudo code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
-let city = document.getElementById("usercity");
-let submitBtn = document.querySelector(".btn");
 let nameCityEl = document.querySelector("#cityname");
-var weatherContainerEl = document.querySelector("#weather");
+let weatherContainerEl = document.querySelector("#weather");
+let submitBtn = document.querySelector(".btn");
+let currentForcast = document.querySelector("#current-forcast");
+let fiveDaysForcast = document.querySelector("#five-days-forcast");
 
 const myAPIKey = "78d9272bcac95b3738e5612909e959e6";
 // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
@@ -12,10 +13,10 @@ let formSubmitHandler = function(event) {
    event.preventDefault();
 
    //get value from input element
-   let city = nameCityEl.ariaValueMax.trim();
+   let usercity = nameCityEl.value.trim();
 
-   if (city) {
-     getCityWeather(city);
+   if (usercity) {
+     getCityWeather(usercity);
 
      // clear old content
      weatherContainerEl.value="";
@@ -25,32 +26,54 @@ let formSubmitHandler = function(event) {
    }
 };
 
-
-// add event lister to the submit button
-submitBtn.addEventListener("click", function () {
-  // format of the weather api url
-  let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myAPIKey}&units=imperial`;
-
-// make a get request to url
-fetch(queryURL).then(function(response) {
-    // request was successful
-    if (response.ok) {
+let getCityWeather = function(city) {
+  // format the open weather api url
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myAPIKey}&units=imperial`;
+  
+  // make a get request to url
+  fetch(apiUrl)
+    .then(function(response) {
+      // request was successful
+      if (response.ok) {
       console.log(response);
       response.json().then(function(data) {
-        // console(data);
         console.log(data);
-        //call display the weather data
-        // displayWeather(data);
+        //call the displayWeather function
+        displayWeather(data, city);
       });
+    }else{
+      alert('Error: City note found');
     }
-    else {
-      console.log(response);
-      alert("There was a problem with your request!");
-    }
+ })
+  .catch(function(error) {
+    alert('Unable to connect to Open Weather');
   });
-}); 
+};
 
-let displayWeather = function(weather) {
+let displayWeather = function(city, searchCity) {
+  // check if api returned any city
+  if (city.length === 0) {
+    weatherContainerEl.textContent = "City not found.";
+    return;
+  }
 
+  currentForcast.textContent = searchCity;
 
+  // city found, push the search city to the history box
+  // when click on a search history, it calls the submit function and displays the weather data.
 }
+
+// on the current forcast section with border and radius
+// display the City enter  + (current date) + weather icon
+// display label "Temp:" + current temperature + "degree icon" + F 
+// display label "Wind:" + current wind + MPH
+// display label Humidity: current + %
+// display UV Index: current number
+
+// on the 5-day Forcast section
+// display 5 cards from today's date.
+// on each card show the date in dd/mm/yyyy format
+// the weather icon
+// Temp:
+// Wind:
+// Humidity:
