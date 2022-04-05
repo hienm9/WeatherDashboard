5bec29c6bed1ab4b898c6f6cbb35156a2d5c8a23
hienm9 <98295316+hienm9@users.noreply.github.com>
2022-04-04 21:43:08 -0400

    update function display weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,7 @@ let userFormEl = document.querySelector("#user-form");
 let nameCityEl = document.querySelector("#cityname");
 let weatherContainerEl = document.querySelector("#weather");
 // let submitBtn = document.querySelector(".btn");
-let currentForcast = document.querySelector("#current-forcast");
-let fiveDaysForcast = document.querySelector("#five-days-forcast");
+
 
 // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 // let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
@@ -30,7 +29,7 @@ let formSubmitHandler = function(event) {
 
 let getCityWeather = function(city) {
   // const myAPIKey = "78d9272bcac95b3738e5612909e959e6";
-  const myAPIKey = "723d63948977f3325b374c286817003e";
+  const myAPIKey = "f186c057bcef67933489204c248dcd30";
   // format the open weather api url
   debugger;
   var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid="+ myAPIKey + "&cnt=5&units=imperial";
@@ -42,7 +41,7 @@ let getCityWeather = function(city) {
       if (response.ok) {
       console.log(response);
       response.json().then(function(data) {
-        console.log(data);
+        // console.log(data);
         //call the displayWeather function
         displayWeather(data);
       });
@@ -69,15 +68,24 @@ let displayWeather = function(searchCity) {
     weatherContainerEl.textContent = "City not found.";
     return;
   }
-  let forecastEl = document.getElementsByClassName("forecast");
+  let fiveDayForecast = document.querySelector("#five-days-forecast");
+  let currentForecast = document.querySelector("#current-forecast");
+  let currentDate = new Date(data.list[i].dt * 1000).toString();
+    currentForecast.innerHTML = currentDate;
 
-  // reset the forcast data to blank
-  // forecastEl[0].classList.add('loaded');
+  let forecastDay = `<div class="forecast-day">
+  <p>${dayname}</p>
+  <p><span class="ico-${icon}" title="${icon}"></span></p>
+  <div class="forecast-day-temp">${temp}<sup>°F</sup></div>
+  <div class="forecast-day-desc">${weatherDescription}</div>
+  </div>`;
+  currentForecast.insertAdjacentHTML('afterend', forecastDay);
+
+
+  // to do reset the forcast data to blank
 
   for (let i = 0; i < 5; i++) {
       
-    console.log('display value: --->'+(searchCity.list[i]) + ": i>>: " + i);
-
         let dayname = new Date(searchCity.list[i].dt * 1000).toLocaleDateString("en", {weekday: "long",});
 
         let icon = searchCity.list[i].weather.icon;
@@ -85,14 +93,14 @@ let displayWeather = function(searchCity) {
         let weatherDescription = searchCity.list[i].weather[0].description;
      
         console.log(" temp: " + temp + " dayname:" + dayname + " weatherDescription:" + weatherDescription);
-        forcastDay = `<div class="forecast-day">
-          <p>${dayname}</p>
-          <p><span class="ico-${icon}" title="${icon}"></span></p>
-          <div class="forecast-day-temp">${temp}<sup>°F</sup></div>
-          <div class="forecast-day-desc">${weatherDescription}</div>
-        </div>`;
+      //  let forecastDay = `<div class="forecast-day">
+      //     <p>${dayname}</p>
+      //     <p><span class="ico-${icon}" title="${icon}"></span></p>
+      //     <div class="forecast-day-temp">${temp}<sup>°F</sup></div>
+      //     <div class="forecast-day-desc">${weatherDescription}</div>
+      //   </div>`;
         // display the forcast for 5 days
-        forecastEl[0].insertAdjacentHTML('afterend', forcastDay);
+        fiveDayForecast[0].insertAdjacentHTML('afterend', forecastDay);
     }
 
 
